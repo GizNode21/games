8c9941fadd6ca41c790d0ddcea7fae77b70d81c4
Jeffrey <jcizek17@jcu.edu>
2024-04-30 21:04:32 -0400

    Second Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+    <div id="game">
+        <Ga_me />
+    </div>
+    <div id="border" />
+    <div id="input" >
+        <Ch_at />
+    </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Ch_at from './components/Ch_at.vue';
+    import Ga_me from './components/Ga_me.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+      name: 'App',
+      components: {
+        Ch_at,
+        Ga_me
+      }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: "Trebuchet MS", Helvetica, Arial, "sans-serif";
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    background-color: black;
+    color: cyan;
+    display: flex;
+  }
+  #game {
+    width: 50vw;
+    height: 100vh;
+  }
+  #border {
+    border-right: 2px solid cyan;
+  }
+  #input {
+    width: 50vw;
+    height: 50vh;
+  }
+  @media (max-width: 1000px) {
+    #app {
+      flex-direction: column;
+    }
+    #game {
+      width: 100vw;
+      height: 50vh;
+    }
+    #input {
+      width: 100vw;
+      height: 50vh;
+    }
+  }
 </style>
